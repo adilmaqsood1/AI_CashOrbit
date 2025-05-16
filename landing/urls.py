0976405ec3d97a8154671ada3bebd3c15fb15b60
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-05-16 22:36:07 +0500

    features page added

--- landing/urls.py
+++ landing/urls.py
@@ -3,4 +3,18 @@ from . import views
 
 urlpatterns = [
     path('', views.LandingPageView.as_view(), name='landing'),
+    path('features/', views.FeaturesPageView.as_view(), name='features'),
+    path('features/advanced-user-rights/', views.FeatureAdvancedUserRightsView.as_view(), name='feature_advanced_user_rights'),
+    path('features/assembly/', views.FeatureAssemblyView.as_view(), name='feature_assembly'),
+    path('features/batch-expiry/', views.FeatureBatchExpiryView.as_view(), name='feature_batch_expiry'),
+    path('features/backups-security/', views.FeatureBackupsSecurityView.as_view(), name='feature_backups_security'),
+    path('features/business-analysis/', views.FeatureBusinessAnalysisView.as_view(), name='feature_business_analysis'),
+    path('features/cloud/', views.FeatureCloudView.as_view(), name='feature_cloud'),
+    path('features/easy-setup/', views.FeatureEasySetupView.as_view(), name='feature_easy_setup'),
+    path('features/tax/', views.FeatureTaxView.as_view(), name='feature_tax'),
+    path('features/inventory/', views.FeatureInventoryView.as_view(), name='feature_inventory'),
+    path('features/multi-currency/', views.FeatureMultiCurrencyView.as_view(), name='feature_multi_currency'),
+    path('features/purchases/', views.FeaturePurchasesView.as_view(), name='feature_purchases'),
+    path('features/sales/', views.FeatureSalesView.as_view(), name='feature_sales'),
+    path('feature_ai_insights/', views.FeatureAIInsightsView.as_view(), name='feature_ai_insights')
 ]
--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,46 @@ from django.views.generic import TemplateView
 
 class LandingPageView(TemplateView):
     template_name = 'landing/home.html'
+
+class FeaturesPageView(TemplateView):
+    template_name = 'landing/features.html'
+
+class FeatureAdvancedUserRightsView(TemplateView):
+    template_name = 'landing/feature_advanced_user_rights.html'
+
+class FeatureAssemblyView(TemplateView):
+    template_name = 'landing/feature_assembly.html'
+
+class FeatureBatchExpiryView(TemplateView):
+    template_name = 'landing/feature_batch_expiry.html'
+
+class FeatureBackupsSecurityView(TemplateView):
+    template_name = 'landing/feature_backups_security.html'
+
+class FeatureBusinessAnalysisView(TemplateView):
+    template_name = 'landing/feature_business_analysis.html'
+
+class FeatureCloudView(TemplateView):
+    template_name = 'landing/feature_cloud.html'
+
+class FeatureEasySetupView(TemplateView):
+    template_name = 'landing/feature_easy_setup.html'
+
+class FeatureTaxView(TemplateView):
+    template_name = 'landing/feature_tax.html'
+
+class FeatureInventoryView(TemplateView):
+    template_name = 'landing/feature_inventory.html'
+
+class FeatureMultiCurrencyView(TemplateView):
+    template_name = 'landing/feature_multi_currency.html'
+
+class FeaturePurchasesView(TemplateView):
+    template_name = 'landing/feature_purchases.html'
+
+class FeatureSalesView(TemplateView):
+    template_name = 'landing/feature_sales.html'
+    
+
+class FeatureAIInsightsView(TemplateView):
+    template_name = 'landing/feature_ai_insights.html'
