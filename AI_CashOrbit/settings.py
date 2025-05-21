7ca91c4eb1292ee27212a565de3230b825da1323
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-05-21 22:30:32 +0500

    allowed host set

--- AI_CashOrbit/settings.py
+++ AI_CashOrbit/settings.py
@@ -16,7 +16,7 @@ from urllib.parse import urlparse
 
 load_dotenv()
 
-DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
+DEBUG = False
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-h*ux4_nqet!-^w4!uewv+mw-8c+9pke^zus67(ol)0w!^+tsg9')
+SECRET_KEY = 'django-insecure-h*ux4_nqet!-^w4!uewv+mw-8c+9pke^zus67(ol)0w!^+tsg9'
 
 
 
